59ecdb351c1c2e66ad2a5055b1b0fd5db3d817c7
Venetia <venetiafaber@gmail.com>
2026-02-17 17:35:58 +0300

    feat: initial server setup with express

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend! ');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>This is an AI chat application</h1>');
+});
+
+app.get('/api/time', (req, res) => {
+  const currentTime = new Date().toLocaleString();
+  res.send(`Current Time: ${currentTime}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
+
+export default app;
